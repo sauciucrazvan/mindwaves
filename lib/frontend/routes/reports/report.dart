010bbb463e9a799a00117f8c5abcffd0eaf3cf1c
Razvan <razvansau9@gmail.com>
2023-09-27 17:28:54 +0300

    Added a bar graph on the weekly report page

--- lib/frontend/routes/reports/report.dart
+++ lib/frontend/routes/reports/report.dart
@@ -1,5 +1,7 @@
 // Generic imports
 import 'package:flutter/material.dart';
+import 'package:graphic/graphic.dart';
+import 'package:mindwaves/frontend/config/palette.dart';
 
 // Frontend imports
 import 'package:mindwaves/frontend/widgets/buttons/leading_button.dart';
@@ -38,6 +40,37 @@ class WeeklyReport extends StatelessWidget {
               ),
 
               const SizedBox(height: 16),
+
+              // Weekly report graph
+              SizedBox(
+                height: 150,
+                width: MediaQuery.of(context).size.width - 50,
+                child: Chart(
+                  data: const [
+                    {'day': 'Mon', 'score': 4},
+                    {'day': 'Tue', 'score': 1},
+                    {'day': 'Wed', 'score': 3},
+                    {'day': 'Thu', 'score': 4},
+                    {'day': 'Fri', 'score': 1},
+                    {'day': 'Sat', 'score': 2},
+                    {'day': 'Sun', 'score': 5},
+                  ],
+                  variables: {
+                    'day': Variable(
+                      accessor: (Map map) => map['day'] as String,
+                    ),
+                    'score': Variable(
+                      accessor: (Map map) => map['score'] as num,
+                    ),
+                  },
+                  marks: [
+                    IntervalMark(
+                      color: ColorEncode(variable: 'score', values: moodColors),
+                    )
+                  ],
+                  axes: [Defaults.horizontalAxis],
+                ),
+              ),
             ],
           ),
         ),
