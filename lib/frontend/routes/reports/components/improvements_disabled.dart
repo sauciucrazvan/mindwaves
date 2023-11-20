be076c8ad9fe58fac74e75e53d18a85fdd221f7c
Razvan Sauciuc <razvansau9@gmail.com>
2023-11-20 20:19:42 +0200

    Redesigned the AI Improvements off dialog. Added a simple animation.

--- lib/frontend/routes/reports/components/improvements_disabled.dart
+++ lib/frontend/routes/reports/components/improvements_disabled.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class DisabledImprovementsError extends StatelessWidget {
   const DisabledImprovementsError({super.key});
@@ -15,12 +16,31 @@ class DisabledImprovementsError extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 12),
-        Text(
-          "AI Improvements is turned off in the settings. Turn it on to receieve recommendations!",
-          style: TextStyle(
-            color: Colors.red[400],
-            fontSize: 15,
-            fontWeight: FontWeight.bold,
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Lottie.asset("assets/animations/ai.json",
+                    width: 128, height: 128),
+                Text(
+                  "AI Improvements is turned off in the settings. Turn it on to receieve recommendations!",
+                  style: TextStyle(
+                    color: Colors.red[400],
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
         ),
       ],
